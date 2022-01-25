ab68378988937c873692e1559b9dfa16d7fbd608
Jamie Lou <jlou711@googlemail.com>
2022-01-25 22:23:23 +0000

    Add fontawesome icons, general styling and loading spinner

--- src/components/AddSongPreview/AddSongPreview.tsx
+++ src/components/AddSongPreview/AddSongPreview.tsx
@@ -1,3 +1,8 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faCheckSquare,
+  faCheckCircle,
+} from "@fortawesome/free-solid-svg-icons";
 import { ITrack } from "../../interfaces/ITrack";
 import "./AddSongPreview.css";
 
@@ -11,30 +16,42 @@ function AddSongPreview({
   addTrackDetails,
 }: IAddSongPreview): JSX.Element {
   return (
-    <div className="container add-song-preview">
-      <div className="preview-album-art">
-        <img
-          src={song.album_art}
-          className="img-fluid rounded-start rounded-end"
-          alt={`album art for ${song.album}`}
+    <>
+      <div className="add-song-preview-header">
+        <h5>Click the tick if this looks right!</h5>
+        <FontAwesomeIcon
+          icon={faCheckCircle}
+          className="confirm-song-btn"
+          size="lg"
+          onClick={addTrackDetails}
         />
       </div>
-      <div className="card-body">
-        <h4 className="card-title">{song.name}</h4>
-        <p className="card-text">Album: {song.album}</p>
-        <p className="card-text">
-          Artists: {song.artists.map((artist) => artist.artist_name).join(", ")}
-        </p>
-        <p className="card-text">
-          <small className="text-muted">
-            Release Date: {song.release_date}
-          </small>
-        </p>
-        <p className="card-text">
-          <small className="text-muted">Popularity: {song.popularity}</small>
-        </p>
+      <div className="container add-song-preview">
+        <div className="preview-album-art">
+          <img
+            src={song.album_art}
+            className="img-fluid rounded-start rounded-end"
+            alt={`album art for ${song.album}`}
+          />
+        </div>
+        <div className="card-body">
+          <h4 className="card-title">{song.name}</h4>
+          <p className="card-text">Album: {song.album}</p>
+          <p className="card-text">
+            Artists:{" "}
+            {song.artists.map((artist) => artist.artist_name).join(", ")}
+          </p>
+          <p className="card-text">
+            <small className="text-muted">
+              Release Date: {song.release_date}
+            </small>
+          </p>
+          <p className="card-text">
+            <small className="text-muted">Popularity: {song.popularity}</small>
+          </p>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
--- src/components/TinderContainer/TinderContainer.tsx
+++ src/components/TinderContainer/TinderContainer.tsx
@@ -1,3 +1,5 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSync } from "@fortawesome/free-solid-svg-icons";
 import axios from "axios";
 import { useCallback, useEffect, useState } from "react";
 import TinderCard from "react-tinder-card";
@@ -9,6 +11,7 @@ function TinderContainer(): JSX.Element {
   const [lastDirection, setLastDirection] = useState<string>();
   const [songList, setSongList] = useState<ITrackDB[]>();
   const [currentSong, setCurrentSong] = useState<ITrackDB>();
+  const [loading, setLoading] = useState(true);
 
   const getSongList = useCallback(async () => {
     // Api call for retrieving token
@@ -28,7 +31,12 @@ function TinderContainer(): JSX.Element {
       songList.pop();
       setSongList([...songList]);
       setCurrentSong(songList[songList.length - 1]);
+      setLoading(true);
     }
+  };
+
+  const hideSpinner = () => {
+    setLoading(false);
   };
 
   useEffect(() => {
@@ -69,25 +77,34 @@ function TinderContainer(): JSX.Element {
       ) : (
         <h2 className="infoText">Swipe to like or dislike a song</h2>
       )}
+      {loading ? (
+        <FontAwesomeIcon
+          icon={faSync}
+          className="loading-spinner fa-spin"
+          size="2x"
+        />
+      ) : null}
       {currentSong ? (
         <iframe
           id="spotify-iframe"
           title="spotify-player"
           src={`https://open.spotify.com/embed/track/${currentSong.uri}`}
-          width="100%"
+          width="30%"
           height="80"
           frameBorder="0"
           allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+          onLoad={hideSpinner}
         ></iframe>
       ) : (
         <iframe
           id="spotify-iframe"
           title="spotify-player"
           src="about:blank"
-          width="100%"
+          width="30%"
           height="80"
           frameBorder="0"
           allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+          onLoad={hideSpinner}
         ></iframe>
       )}
     </div>
