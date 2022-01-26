7caa94f278399c00cebdb5cf9348c2b10bcc0e2a
Jamie Lou <jlou711@googlemail.com>
2022-01-26 22:26:22 +0000

    Fix capitalize test

--- src/utils/capitalize.test.ts
+++ src/utils/capitalize.test.ts
@@ -1,7 +1,7 @@
 import { capitalize } from "./capitalize";
 
 test("capitalize returns a string, capitalizing the first letter", () => {
-  expect(capitalize("world")).toBe("World!");
-  expect(capitalize("richard")).toBe("Richard!");
-  expect(capitalize("academy scholars")).toBe("Academy scholars!");
+  expect(capitalize("world")).toBe("World");
+  expect(capitalize("richard")).toBe("Richard");
+  expect(capitalize("academy scholars")).toBe("Academy scholars");
 });
